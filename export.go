3bf3fef47f7bc64e6e42eda40f8c8812c312de24
Volker Dobler <dr.volker.dobler@gmail.com>
2014-03-12 14:00:17 +0100

    Add license information.

--- export.go
+++ export.go
@@ -1,4 +1,6 @@
-// Copyright Volker Dobler 2014
+// Copyright 2014 Volker DObler. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
 
 // Package export provides tools to dump tabulated data.
 //
